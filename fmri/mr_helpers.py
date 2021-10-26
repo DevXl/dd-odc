4301f124f007887688c930faf4e2378f801a72cd
devxl <sharif.saleki@gmail.com>
2021-10-25 23:10:23 -0400

    changed parameter name and added projector (still needs work)

--- fmri/mr_helpers.py
+++ fmri/mr_helpers.py
@@ -2,10 +2,9 @@
 """
 Helper functions for double-drift experiments
 """
-from psychopy import logging
 
 
-def setup_path(sub, root, task):
+def setup_path(sub, root, exp_part):
     """
     Sets up paths and directories for experiment
     The structure loosely follows BIDS convention
@@ -15,7 +14,7 @@ def setup_path(sub, root, task):
     sub : int
     root : Pathlib object
         points to the root directory of the experiment
-    task : str
+    exp_part : str
     """
     # data directory
     data_dir = root / "data"
@@ -31,7 +30,7 @@ def setup_path(sub, root, task):
         sub_dir.mkdir()
 
     # task directory
-    task_dir = sub_dir / task
+    task_dir = sub_dir / exp_part
     if not task_dir.exists():
         print("Making the task directory...")
         task_dir.mkdir()
@@ -65,11 +64,11 @@ def get_monitors(mon_name):
             "dist": 60,
             "refresh_rate": 165
         },
-        "Asus": {
+        "projector": {
             "size_cm": (54, 30),
-            "size_px": (1920, 1080),
-            "dist": 57,
-            "refresh_rate": 240
+            "size_px": (1280, 960),
+            "dist": 124,
+            "refresh_rate": 60
         }
     }
 
