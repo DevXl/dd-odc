54a9ef4c72f66a88d117cf9c5b1a463456c4b40e
Sharif Saleki <sharif.saleki@gmail.com>
2021-11-04 22:04:44 -0400

    added windows serial path

--- fmri/testing.py
+++ fmri/testing.py
@@ -7,7 +7,7 @@ from mr_helpers import setup_path, get_monitors
 from pygaze import eyetracker, libscreen
 import pygaze
 
-mon_name = 'Asus'
+mon_name = 'projector'
 mon_specs = get_monitors(mon_name)
 exp_mon = monitors.Monitor(name=mon_name, width=mon_specs["size_cm"][0], distance=mon_specs["dist"])
 exp_mon.setSizePix(mon_specs["size_px"])
@@ -37,8 +37,8 @@ waiting = visual.TextStim(exp_win, pos=[0, 0], text="Waiting for scanner...",
 waiting_fake = visual.TextStim(exp_win, pos=[0, 0], text="Waiting for (fake) scanner...",
                                color='black', name="Waiting_fake")
 
-
-serial_path = '/dev/cu.USA19H62P1.1'
+serial_path = 'COM3'
+# serial_path = '/dev/cu.USA19H62P1.1'
 # serial_path = '/dev/cu.USA19H142P1.1'
 # serial_path = '/dev/tty.USA19H142P1.1'
 
@@ -48,7 +48,6 @@ if not exists(serial_path):
     serial_exists = False
     b_serial = "No serial device detected, using keyboard"
     event.waitKeys(keyList=['5'])
-
     first_trigger = "Got sync from keyboard. Resetting clocks"
 else:
     waiting.draw()
@@ -63,7 +62,7 @@ else:
         ser_out = ser.read()
         if b'5' in ser_out:
             scanner_wait = False
-            first_trigger = "Got sync from scannner!"
+            first_trigger = "Got sync from scanner!"
 
 # check keys
 print(b_serial)
